75aa1e406d7c7f61a41c324f2ea7f78278db37ec
rishabhjha-1 <rishabhjha0@gmail.com>
2026-08-14 15:07:43 +0530

    Add function to find missing numbers in array
    Implement findDisappearedNumbers function to find missing integers in an array.

--- index.js
+++ index.js
@@ -1245,6 +1245,32 @@ var subarraySum = function(nums, k) {
 	}
 
 
+// Given an array nums of n integers where nums[i] is in the range [1, n], return an array of all the integers in the range [1, n] that do not appear in nums.
+
+ 
+
+Example 1:
+
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+Example 2:
+
+Input: nums = [1,1]
+Output: [2]
+var findDisappearedNumbers = function(nums) {
+    for(let i=0;i<nums.length;i++){
+        let num=Math.abs(nums[i])
+        nums[num-1]= -Math.abs(nums[num-1])
+    }
+    let result=[]
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>0) result.push(i+1)
+    }
+    return result
+    
+};
+
+
 /*If current element is not val, copy it to position k, then increment k.
 
 After the loop ends, the first k elements of nums will be all elements that are not equal to val.
