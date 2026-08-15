39c3265762b4ebe34e0c1a09cc2c05f834d07769
rishabhjha-1 <rishabhjha0@gmail.com>
2026-08-15 15:40:51 +0530

    Add productExceptSelf function implementation
    Implement productExceptSelf function to return an array of products of all elements except self without division.

--- index.js
+++ index.js
@@ -1271,6 +1271,37 @@ var findDisappearedNumbers = function(nums) {
 };
 
 
+// Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+// The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+// You must write an algorithm that runs in O(n) time and without using the division operation.
+
+ 
+
+// Example 1:
+
+// Input: nums = [1,2,3,4]
+// Output: [24,12,8,6]
+
+
+var productExceptSelf = function(nums) {
+    let n=nums.length
+    let ans = new Array(n).fill(1)
+    prefix=1
+    for(let i=0;i<n;i++){
+        ans[i]=prefix
+        prefix*=nums[i]
+    }
+    let suffix=1
+    for(let i=n-1;i>=0;i--){
+        ans[i]= ans[i] * suffix
+        suffix *= nums[i]
+    }
+    return ans
+};
+
+
 /*If current element is not val, copy it to position k, then increment k.
 
 After the loop ends, the first k elements of nums will be all elements that are not equal to val.
