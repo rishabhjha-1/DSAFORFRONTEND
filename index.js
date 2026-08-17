c4802a11f83b3207eea26fa20f0037a328d150a6
rishabhjha-1 <rishabhjha0@gmail.com>
2026-08-17 13:14:39 +0530

    Add threeSum function for zero-sum triplet detection
    Implemented threeSum function to find triplets that sum to zero.

--- index.js
+++ index.js
@@ -1355,6 +1355,52 @@ var longestConsecutive = function(nums) {
 };
 
 
+
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+
+    const result = [];
+
+    for (let i = 0; i < nums.length - 2; i++) {
+
+        // Skip duplicate first numbers
+        if (i > 0 && nums[i] === nums[i - 1]) {
+            continue;
+        }
+
+        let left = i + 1;
+        let right = nums.length - 1;
+
+        while (left < right) {
+            const sum = nums[i] + nums[left] + nums[right];
+
+            if (sum === 0) {
+                result.push([nums[i], nums[left], nums[right]]);
+
+                // Skip duplicate left/right values
+                while (left < right && nums[left] === nums[left + 1]) {
+                    left++;
+                }
+
+                while (left < right && nums[right] === nums[right - 1]) {
+                    right--;
+                }
+
+                left++;
+                right--;
+
+            } else if (sum < 0) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+    }
+
+    return result;
+};
+
+
 /*If current element is not val, copy it to position k, then increment k.
 
 After the loop ends, the first k elements of nums will be all elements that are not equal to val.
