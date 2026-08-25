7d2ada1ebe7980c550d5fcf938052f4d76be0150
rishabhjha-1 <rishabhjha0@gmail.com>
2026-08-25 22:52:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1545,6 +1545,131 @@ reverse() {
   this.head = prev;
 }
 
+
+
+
+
+
+//sliding windows
+
+// There is an integer array nums sorted in ascending order (with distinct values).
+
+// Prior to being passed to your function, nums is possibly left rotated at an unknown index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be left rotated by 3 indices and become [4,5,6,7,0,1,2].
+
+// Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+// You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+// Example 1:
+
+// Input: nums = [4,5,6,7,0,1,2], target = 0
+// Output: 4
+//fixed window
+
+
+var findMaxAverage = function(nums, k) {
+    let windowSum = 0;
+    let maxSum = -Infinity;
+
+    let left = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+
+        windowSum += nums[right];
+
+        // Window size becomes k
+        if (right - left + 1 === k) {
+
+            maxSum = Math.max(maxSum, windowSum/k);
+
+            // Remove left element
+            windowSum -= nums[left];
+
+            // Move left
+            left++;
+        }
+    }
+
+    return maxSum;
+};
+
+
+
+// Given a string s, find the length of the longest substring without duplicate characters.
+
+ 
+
+// Example 1:
+
+// Input: s = "abcabcbb"
+// Output: 3
+// Explanation: The answer is "abc", with the length of 3. Note that "bca" and "cab" are also correct answers.
+//dynamic window
+
+var lengthOfLongestSubstring = function(s) {
+    let left=0
+    let maxLen=0
+    let set=new Set()
+    for(let i=0;i<s.length;i++){
+        while(set.has(s[i])){
+            set.delete(s[left])
+            left++
+
+        }
+        set.add(s[i])
+        maxLen=Math.max(maxLen,i-left+1)
+    }
+    return maxLen
+};
+
+
+
+//dynamic window
+
+var numSubarrayProductLessThanK = function(nums, k) {
+    if (k <= 1) return 0;
+    let left=0
+    let count=0
+    let multi=1
+    for(let right =0 ; right<nums.length; right++){
+        multi*=nums[right]
+        while (multi>=k){
+  
+            multi=multi/nums[left]
+            left++
+        }
+         count += right - left + 1;
+
+    }
+    return count
+};
+
+
+
+var minSubArrayLen = function(target, nums) {
+    let left = 0;
+    let minLen = Infinity;
+    let sum = 0;
+
+    for (let right = 0; right < nums.length; right++) {
+        sum += nums[right];
+
+        while (sum >= target) {
+            minLen = Math.min(minLen, right - left + 1);
+
+            sum -= nums[left];
+            left++;
+        }
+    }
+
+    return minLen === Infinity ? 0 : minLen;
+};
+
+
+
+
 /*If current element is not val, copy it to position k, then increment k.
 
 After the loop ends, the first k elements of nums will be all elements that are not equal to val.
